043c003428df98c3163419dcbcbfb3efb7786aee
FairyHome <2095215135@qq.com>
2023-05-30 21:57:37 +0800

    拦截器优化（访问任何页面多刷新token过期值）

--- src/main/java/com/hmdp/config/InterceptorConfig.java
+++ src/main/java/com/hmdp/config/InterceptorConfig.java
@@ -1,6 +1,7 @@
 package com.hmdp.config;
 
 import com.hmdp.interceptor.loginInterceptor;
+import com.hmdp.interceptor.refreshTokenInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -30,7 +31,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         //创建自定义拦截器对象
         //使用redis+token完成用户验证时需要StringRedisTemplate,在这里注入的
-        final loginInterceptor loginInterceptor = new loginInterceptor(stringRedisTemplate);
+        final loginInterceptor loginInterceptor = new loginInterceptor();
         registry.addInterceptor(loginInterceptor).excludePathPatterns(
                 "/shop/**",
                 "/voucher/**",
@@ -40,7 +41,12 @@ public class InterceptorConfig implements WebMvcConfigurer {
                 "/user/code",
                 "/user/login"
         ).order(1);
+        registry.addInterceptor(new refreshTokenInterceptor(stringRedisTemplate)).addPathPatterns("/**").order(0); //默认拦截所有请求，需要保证这个拦截器在最前面
 
 //        WebMvcConfigurer.super.addInterceptors(registry);   通过类名.super.方法可以名可以调用接口中的default方法
+
+
     }
+
+
 }
