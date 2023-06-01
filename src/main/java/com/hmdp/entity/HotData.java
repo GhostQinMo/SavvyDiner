d25b2c8de4d2f3b63da26f9c3bc12fc638104b84
FairyHome <2095215135@qq.com>
2023-06-01 17:26:34 +0800

    商铺缓存优化：使用互斥锁解决缓存击穿（未涉及到多个分布式情况，而是单体应用）

---
+++ src/main/java/com/hmdp/entity/HotData.java
@@ -0,0 +1,22 @@
+package com.hmdp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Black_ghost
+ * @title: HotData
+ * @projectName AngelXin_dianping
+ * @description :616  An unchanging God  Qin_Love
+ * @vesion 1.0.0
+ * @CreateDate 2023-06-01 17:03:16
+ * @Description 热点数据
+ **/
+@Data
+@AllArgsConstructor
+public class HotData {
+    LocalDateTime expireDataLogic;
+    Object object;
+}
--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,9 +1,11 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.Result;
+import com.hmdp.entity.HotData;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
@@ -37,8 +40,21 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     @Override
     public Result selectById(Long id) {
+        //缓存穿透
+//        return CachePenetrate(id);
+
+        //使用互斥锁解决缓存击穿
+        Result result = CacheBreakdown(id);
+        if (result == null) {
+            log.info("查询结果为null");
+        }
+        return result;
+    }
+
+    //提取缓存穿透
+    private  Result CachePenetrate(Long id ){
         // 2. id是唯一的，所以是根据商铺id为key来保存商品信息的，这里试着采用json字符串形式来保存商铺信息，这里是只是尝试，可以使用map数据结构的
-         String shopjson = stringRedisTemplate.opsForValue().get(RedisConstants.CACHE_SHOP_KEY +id.toString());
+        String shopjson = stringRedisTemplate.opsForValue().get(RedisConstants.CACHE_SHOP_KEY +id.toString());
 
         //3. 如果命中则直接返回商铺信息
         if (!StrUtil.isBlankIfStr(shopjson)){
@@ -63,7 +79,7 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
                     "",
                     RedisConstants.CACHE_NULL_TTL,
                     TimeUnit.MINUTES);
-           return  Result.fail("商品不存在");
+            return  Result.fail("商品不存在");
         }
         //6.存在该商铺，则保存商铺信息到redis，然后返回
         stringRedisTemplate.opsForValue().set(
@@ -71,8 +87,120 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
                 JSONUtil.toJsonStr(shopById),
                 RedisConstants.CACHE_SHOP_TTL,
                 TimeUnit.MINUTES
-                );     //优化：添加缓存一致性解决方法的兜底方案添加过期时间
+        );     //优化：添加缓存一致性解决方法的兜底方案添加过期时间
         return Result.ok(shopById);
+    }
+
+    //提取缓存击穿(而且缓存穿透也包含)
+    private Result CacheBreakdown(Long id){
+        // 2. id是唯一的，所以是根据商铺id为key来保存商品信息的，这里试着采用json字符串形式来保存商铺信息，这里是只是尝试，可以使用map数据结构的
+        String shopjson = stringRedisTemplate.opsForValue().get(RedisConstants.CACHE_SHOP_KEY +id.toString());
+
+        //3. 如果命中则直接返回商铺信息
+        if (!StrUtil.isBlankIfStr(shopjson)){
+            final Shop shop = JSONUtil.toBean(shopjson, Shop.class);
+            log.info("从缓存中拿去商铺信息：商铺id为{}",id);
+            return Result.ok(shop);
+        }
+        //解决缓存穿透：缓存空串，因为StrUtil.isBlankIfStr(shopjson)会把""也判断为null,所以这里需要再次判断是否为空，只有真的为null采取查询数据库
+        if (!Objects.equals(shopjson, null)) {
+            return  Result.fail("发生缓存穿透，请输入正确的查询条件");
+        }
+
+        /*这里是我自己写的，没有使用递归
+        //4. 没有命中，尝试获取互斥锁，开始重建缓存
+        // TODO 这里可以写成double-check
+        while(true){
+            //这里为什么不需要考虑线程安全问题？redis为我们保证了
+            if (!tryMutexLock(id.toString())){
+                try {
+                    //获取锁失败则休眠一段时间，然后在尝试获取
+                    TimeUnit.MILLISECONDS.sleep(500);
+                    // TODO 这里应该可以再去查询缓存如果有了，则直接返回
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }else {
+                //命中
+                break;
+            }
+        }*/
+         Shop shopById=null;
+
+        try {
+            //4. 没有命中，尝试获取互斥锁，开始重建缓存
+            //4.1 如果没有获取锁成功则休眠一段时间
+            if(!tryMutexLock(id.toString())){
+                TimeUnit.MILLISECONDS.sleep(200);
+                //再次判断
+                return CacheBreakdown(id);
+            }
+            //4.2 如果获取成功，则进入查询数据库
+            //  做duble-check
+            shopjson = stringRedisTemplate.opsForValue().get(RedisConstants.CACHE_SHOP_KEY + id);
+            if (!StrUtil.isBlankIfStr(shopjson)){
+                final Shop shop = JSONUtil.toBean(shopjson, Shop.class);
+                log.info("从缓存中拿去商铺信息：商铺id为{}",id);
+                return Result.ok(shop);
+            }
+
+            //如果duble-check失败，加锁，查询数据库
+            shopById = getById(id);
+            // 5. 数据库中不存在则返回内容不存在
+
+            //模拟重构key时需要一些复杂的流程
+            TimeUnit.MILLISECONDS.sleep(200);
+
+            if (!Objects.nonNull(shopById)){
+                //解决缓存穿透：缓存空串
+                stringRedisTemplate.opsForValue().set(
+                        RedisConstants.CACHE_SHOP_KEY+ id,
+                        "",
+                        RedisConstants.CACHE_NULL_TTL,
+                        TimeUnit.MINUTES);
+                return  Result.fail("商品不存在");
+            }
+            //6.存在该商铺，则保存商铺信息到redis，然后返回
+            stringRedisTemplate.opsForValue().set(
+                    RedisConstants.CACHE_SHOP_KEY+ id,
+                    JSONUtil.toJsonStr(shopById),
+                    RedisConstants.CACHE_SHOP_TTL,
+                    TimeUnit.MINUTES
+            );     //优化：添加缓存一致性解决方法的兜底方案添加过期时间
+
+        } catch (InterruptedException e) {
+            log.error("商铺缓存重建失败！！！");
+            e.printStackTrace();
+        }finally{
+            //释放互斥锁
+            releaseMutexLock(id.toString());
+        }
+        //返回
+        return Result.ok(shopById);
+    }
+
+    /**
+     *
+     * @param key 互斥锁对象
+     * @return boolean
+     * @Description 获取互斥锁
+     */
+    private boolean tryMutexLock(String key){
+        final Boolean aBoolean = stringRedisTemplate.opsForValue().setIfAbsent(RedisConstants.LOCK_SHOP_KEY+key, "1", RedisConstants.LOCK_SHOP_TTL, TimeUnit.SECONDS);
+        //这里需要注意自动拆箱问题
+        //因为flag是包装类,而方法返回的是基本数据类型,包装类是可以为null,但基本数据类型不能为null
+        //使用Hutool工具包解决
+        return BooleanUtil.isTrue(aBoolean);
+    }
+
+    /**
+     *
+     * @param key 互斥锁对象
+     * @return void
+     * @Description 释放互斥锁
+     */
+    private void releaseMutexLock(String key){
+        final Boolean ifdel = stringRedisTemplate.delete(RedisConstants.LOCK_SHOP_KEY+key);
     }
 
 
@@ -92,4 +220,24 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
         return Result.ok("更新成功");
     }
+
+    /**
+     *
+     * @param id
+     * @param expireSecond
+     * @Description 添加热点商铺
+     */
+    public void addHotShop(Long id , Long expireSecond){
+        //1.从数据库中查询商铺信息,封装数据
+         Shop shop = getById(id);
+         HotData hotData = new HotData(LocalDateTime.now().plusSeconds(expireSecond), shop);
+         //2.保存到redis中(这是另一种解决缓存击穿的方法)
+        stringRedisTemplate.opsForValue().set(
+                RedisConstants.CACHE_SHOP_KEY+ id,
+                JSONUtil.toJsonStr(hotData)
+        );
+    }
+
+
+
 }
